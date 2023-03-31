86ebbb4632afa6d79acd04aae8102ba8f0642d64
James Kabbes <james.kabbes@gmail.com>
2023-03-31 15:55:52 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     setup(
     package_data={'kabbes_account_manager': 
         [ 
-            'default_settings.json'
+            'CONFIG.json'
         ]
         }
     )
